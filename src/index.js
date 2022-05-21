dd8738281dcefcefc06adcaf55b6f0f04202f530
RomaTomashevskiy <romatomashevskiy@ukr.net>
2022-05-21 18:36:17 +0300

    Домашка

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
 import './css/styles.css';
+import {fetchCountries} from './js/fetchCountries'
+import {countryMarkup} from './js/countryMurkup.js'
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import templateFunction from './templates/countries.hbs'
+var debounce = require('lodash.debounce');
 
+
+
+const refs ={
+    input: document.querySelector('#search-box'),
+    list: document.querySelector('.country-list'),
+    text: document.querySelector('.country-info')
+}
 const DEBOUNCE_DELAY = 300;
+
+refs.input.addEventListener('input', debounce(searchHandler, DEBOUNCE_DELAY));
+
+function searchHandler(evt){
+    const country = (evt.target.value).trim();
+    clearMurkup();
+
+    if (country){
+        fetchCountries(country).then(countries => {
+            
+            if (countries.length === 1){              
+                appendCountries(countries);
+                appendCountryInfo(countries);
+
+            }else if (countries.length >=2 &&  countries.length <= 10){
+                appendCountries(countries);
+                
+
+            }else if (countries.length > 10){
+                Notify.warning("Too many matches found. Please enter a more specific name.");
+            }
+        })
+        .catch(err => {
+            console.dir(err);
+            Notify.failure("Oops, there is no country with that name");
+        })
+    }
+    
+}
+
+
+function appendCountries(countries){
+    refs.list.insertAdjacentHTML('beforeend', templateFunction(countries));
+}
+
+function appendCountryInfo(countries){
+    refs.text.insertAdjacentHTML('beforeend', countryMarkup(countries[0]));
+}
+
+function clearMurkup(){
+    refs.list.innerHTML = '';
+    refs.text.innerHTML = '';
+}
+
+
+Notify.init({
+    warning: {
+        background: '#6291EF',
+    },
+})
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,11 @@
+const params = 'name,capital,population,flags,languages';
+
+export function fetchCountries(name){
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=${params}`)
+    .then(response => {
+        if (!response.ok) throw new Error(response.status);
+
+        return response.json()
+    });
+}
+
